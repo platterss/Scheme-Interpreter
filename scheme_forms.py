8ed6c97d57ae0d32ddf302a37d2a9027f6b71a21
simplehieu <38307012+simplehieu@users.noreply.github.com>
2025-05-10 20:13:41 -0700

    Completed problems 7, 10, 11

--- scheme_forms.py
+++ scheme_forms.py
@@ -229,7 +229,8 @@ def do_mu_form(expressions, env):
     formals = expressions.first
     validate_formals(formals)
     # BEGIN PROBLEM 11
-    "*** YOUR CODE HERE ***"
+    body = expressions.rest
+    return MuProcedure(formals, body)
     # END PROBLEM 11
 
 
